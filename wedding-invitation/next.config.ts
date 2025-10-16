e6d8645366a245834a58276b0530a1c7b882a24d
hyjung <hyjung@ntels.com>
2025-10-16 14:51:09 +0900

    init

--- wedding-invitation/next.config.ts
+++ wedding-invitation/next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
     output: "export",
-    basePath: isProd ? "/hy2jung.github-io" : "",
-    assetPrefix: isProd ? "/hy2jung.github-io/" : "",
+    basePath: isProd ? "/hy2jung.github.io" : "",
+    assetPrefix: isProd ? "/hy2jung.github.io/" : "",
     images: { unoptimized: true },
 };
 
